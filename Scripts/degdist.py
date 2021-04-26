e3e946c9e2c215de379552d4289efe10b6b719e7
keiranworrell <worrellkeiran@gmail.com>
2021-04-26 13:22:36 +0100

    Update degdist.py

--- Scripts/degdist.py
+++ Scripts/degdist.py
@@ -1,60 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-k = [17,25,4,1,3,2,9,11,101,10,7,6,12,5,19,20,14,24,13,23,8,16]
-pk = [2,1,8,53,24,41,2,2,1,4,8,2,2,11,1,2,2,1,2,1,5,1]
-weighted_k = [19,59,4,1,3,2,12,21,252,17,10,9,6,29,33,5,53,41,67,24,22,62,7,8,15,13,25,16,20,11,14]
-weighted_pk = [1,1,11,53,19,37,3,1,1,3,6,3,4,1,1,11,1,1,1,1,2,1,3,2,1,1,1,1,1,1,2]
+k = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,19,20,23,24,25,101]
+pk = [53,41,24,8,11,2,8,5,2,4,2,2,2,2,1,2,1,2,1,1,1,1]
+noShaka_k =  k[:-1]
+noShaka_pk = pk[:-1]
+cumulative_k = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,19,20,23,24,25,101]
+cumulative_pk = [176,123,82,58,50,39,37,29,24,22,18,16,14,12,10,9,7,6,4,3,2,1]
+noShaka_cumulative_k = cumulative_k[:-1]
+noShaka_cumulative_pk = [175,122,81,57,49,38,36,28,23,21,17,15,13,11,9,8,6,5,3,2,1]
 
 log_k = np.log(k)
 log_pk = np.log(pk)
-log_weighted_k = np.log(weighted_k)
-log_weighted_pk = np.log(weighted_pk)
+log_noShaka_k = np.log(noShaka_k)
+log_noShaka_pk = np.log(noShaka_pk)
+log_cumulative_k = np.log(cumulative_k)
+log_cumulative_pk = np.log(cumulative_pk)
+log_noShaka_cumulative_k = np.log(noShaka_cumulative_k)
+log_noShaka_cumulative_pk = np.log(noShaka_cumulative_pk)
 
-# Plot k vs pk
-plt.plot(k, pk, 'ro')
-plt.grid()
-plt.xlabel("$k$")
-plt.ylabel("$p(k)$")
-plt.title("degree $k$ vs proportion $p(k)$,\nwith fitted curve $p(k)=43.5k^{-1.122}$")
-x = np.linspace(0.75,120,1000)
-y = 43.5*(x**-1.122)
-plt.plot(x,y, 'b')
-plt.savefig("degdist.pdf")
-plt.clf()
 
-# Plot weighted k vs weighted pk
-plt.plot(weighted_k, weighted_pk, 'ro')
-plt.grid()
-plt.xlabel("$k_{weighted}$")
-plt.ylabel("$p(k_{weighted})$")
-plt.title("weighted degree $k_{weighted}$ vs proportion $p(k_{weighted})$,\nwith fitted curve $p(k_{weighted})=23.4k^{-0.861}$")
-x = np.linspace(0.5,275,1000)
-y = 23.4*(x**-0.861)
-plt.plot(x,y, 'b')
-plt.savefig("degdist_weighted.pdf")
-plt.clf()
+def k_fitted(x):
+    return(43.5*(x**-1.122))
 
-# Plot log(k) vs log(pk)
-plt.plot(log_k, log_pk, 'ro')
-plt.grid()
-plt.xlabel("$log(k)$")
-plt.ylabel("$log(p(k))$")
-plt.title("log of degree $log(k)$ vs log of proportion $log(p(k))$,\nwith the fitted curve $log(p(k))=-1.122k+3.77$")
-x = np.linspace(0,5,100)
-y = (-1.122*x)+3.77
-plt.plot(x,y, 'b')
-plt.savefig("logdegdist.pdf")
-plt.clf()
+def noShaka_fitted(x):
+    return(67.4*(x**-1.35))
 
-# Plot log(weighted k) vs log(weighted pk)
-plt.plot(log_weighted_k, log_weighted_pk, 'ro')
-plt.grid()
-plt.xlabel("$log(k_{weighted})$")
-plt.ylabel("$log(p(k_{weighted}))$")
-plt.title("log of weighted degree $log(k_{weighted})$ vs log of proportion $log(p(k_{weighted}))$,\nwith the fitted curve $log(p(k_{weighted}))=-0.86k+3.15$")
-x = np.linspace(0,6,100)
-y = (-0.86*x)+3.15
-plt.plot(x,y, 'b')
-plt.savefig("logdegdist_weighted.pdf")
-plt.clf()
+def cumulative_fitted(x):
+    return(330*(x**-1.299))
+
+def cumulative_noShaka_fitted(x):
+    return(405.85*(x**-1.451))
+
+def log_fitted(x):
+    return((-1.122*x)+3.77)
+
+def log_noShaka_fitted(x):
+    return((-1.35*x)+4.21)
+
+def log_cumulative_fitted(x):
+    return((-1.299*x)+5.799)
+
+def log_cumulative_noShaka_fitted(x):
+    return((-1.451*x)+6.006)
+
+xData = [k, noShaka_k, log_k, log_noShaka_k, cumulative_k, log_cumulative_k, noShaka_cumulative_k, log_noShaka_cumulative_k]
+yData = [pk, noShaka_pk, log_pk, log_noShaka_pk, cumulative_pk, log_cumulative_pk, noShaka_cumulative_pk, log_noShaka_cumulative_pk]
+xLabels = ["$k$", "$k_{noShaka}$", "$log(k)$", "$log(k_{noShaka})$", "Cumulative $k$", "$log($cumulative $k)$", "Cumulative $k$ without Shaka", "$log($cumulative $k)$ without Shaka"]
+yLabels = ["$p(k)$", "$p(k_{noShaka})$", "$log(p(k))$", "$log(p(k_{noShaka}))$", "$p($cumulative $k)$", "$log(p($cumulative $k))$", "$p($cumulative $k)$ without Shaka", "$log(p($cumulative $k))$ without Shaka"]
+graphTitles = [
+    "degree $k$ vs proportion $p(k)$,\nwith fitted curve $p(k)=43.5k^{-1.122}$",
+    "degree $k_{noShaka}$ vs proportion $p(k_{noShaka})$,\nwith fitted curve $p(k_{noShaka})=67.4k^{-1.35}$",
+    "log of degree $log(k)$ vs log of proportion $log(p(k))$,\nwith the fitted curve $log(p(k))=-1.122k+3.77$",
+    "log of degree $log(k_{noShaka})$ vs log of proportion $log(p(k_{noShaka}))$,\nwith the fitted curve $log(p(k_{noShaka}))=-1.35k+4.21$",
+    "cumulative degree $k$ vs proportion $p(k)$,\nwith the fitted curve $p(k)=330x^{-1.451}$",
+    "log of degree $log(k)$ vs log of proportion $log(p(k))$,\nwith the fitted curve $log(p(k))=-1.299k+5.799$",
+    "cumulative degree $k$ vs proportion $p(k)$ without Shaka,\nwith the fitted curve $p(k)=405.85x^{-1.299}$",
+    "log of degree $log(k)$ vs log of proportion $log(p(k))$ without Shaka,\nwith the fitted curve $log(p(k))=-1.451k+6$"
+]
+xLowerLims = [0.8,0.8,0,0,1.25,0,1.25,0]
+xUpperLims = [120,30,5,3.5,100,5,26,3.5]
+fittedCurves = [k_fitted, noShaka_fitted, log_fitted, log_noShaka_fitted, cumulative_fitted, log_cumulative_fitted, cumulative_noShaka_fitted, log_cumulative_noShaka_fitted]
+plotTitles = ["degdist.pdf", "degdist_noShaka.pdf", "logdegdist.pdf", "logdegdist_noShaka.pdf", "degdist_cumulative.pdf", "logdegdist_cumulative.pdf", "degdist_cumulative_noShaka.pdf", "logdegdist_cumulative_noShaka.pdf"]
+
+
+def plot(xData, yData, xLabel, yLabel, title, xLowerLims, xUpperLims, fittedCurve, figTitle):
+    plt.plot(xData, yData, 'ro')
+    plt.grid()
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.title(title)
+    x = np.linspace(xLowerLims,xUpperLims,100)
+    y = fittedCurve(x)
+    plt.plot(x,y, 'b')
+    plt.savefig(figTitle)
+    plt.clf()
+
+for i in range(len(xData)):
+    plot(xData[i], yData[i], xLabels[i], yLabels[i], graphTitles[i], xLowerLims[i], xUpperLims[i], fittedCurves[i], plotTitles[i])
